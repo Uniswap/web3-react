928634377026562f9717ea05586a8f012d537fe2
Noah Zinsmeister <noahwz@gmail.com>
2018-11-15 12:17:28 -0500

    improve hooks

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-export const useThrottled = (functionToThrottle, interval) => {
-  const [lastCalled, setLastCalled] = useState(0)
+export const useThrottled = (functionToThrottle, interval, initialLastCalled = 0) => {
+  const [lastCalled, setLastCalled] = useState(initialLastCalled)
 
   return function throttle () {
     const now = Date.now()
--- src/web3Hooks.js
+++ src/web3Hooks.js
@@ -1,17 +1,35 @@
 import { useContext, useState, useEffect } from 'react'
 import { Web3Context } from './index'
 
-export const useAccountBalance = (numberOfDigits = 3) => {
+export function useRerendererEffect(effect, depends) {
   const context = useContext(Web3Context);
+  useEffect(effect, [...depends, context.accountRerenderer, context.accountRerenderer])
+}
 
+export function useAccountBalance ({numberOfDigits = 3, format = 'ether'} = {}) {
+  const context = useContext(Web3Context);
   const [ balance, setBalance ] = useState(undefined)
 
-  useEffect(() => {
-    context.utilities.getBalance(context.account)
+  useRerendererEffect(() => {
+    context.utilities.getBalance(undefined, format)
       .then(balance => {
         setBalance(Number(balance).toLocaleString(undefined, { maximumFractionDigits: numberOfDigits }))
       })
-  }, [context.account, context._accountRerender])
+  }, [context.account])
+
+  return balance
+}
+
+export function useERC20Balance (ERC20Address, numberOfDigits = 3) {
+  const context = useContext(Web3Context);
+  const [ balance, setBalance ] = useState(undefined)
+
+  useRerendererEffect(() => {
+    context.utilities.getERC20Balance()
+      .then(balance => {
+        setBalance(Number(balance).toLocaleString(undefined, { maximumFractionDigits: numberOfDigits }))
+      })
+  }, [context.account])
 
   return balance
 }
