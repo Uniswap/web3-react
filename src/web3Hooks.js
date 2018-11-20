5b0bd11818e2a2b8dcdfacba0f9501105a1d9200
Noah Zinsmeister <noahwz@gmail.com>
2018-11-20 11:46:53 -0500

    introduce useTransactionManager
    fix broken rerenderer logic
    
    add context.networkId to useAccountEffect
    
    swap argument order in useEtherscanLink

--- src/web3Hooks.js
+++ src/web3Hooks.js
@@ -1,9 +1,9 @@
-import { useState, useEffect, useContext, useMemo } from 'react'
+import { useState, useEffect, useContext, useReducer, useMemo } from 'react'
 
 import Web3Context from './Web3Context'
 import {
   getNetworkName, getEtherscanLink, getAccountBalance, getERC20Balance,
-  signPersonal, sendTransaction, TRANSACTION_ERRORS
+  signPersonal, sendTransaction, TRANSACTION_ERROR_CODES
 } from './web3Utilities'
 
 export function useWeb3Context () {
@@ -15,21 +15,21 @@ export function useNetworkName (networkId) {
   return useMemo(() => getNetworkName(networkId || context.networkId), [networkId, context.networkId])
 }
 
-export function useEtherscanLink (networkId, type, data) {
+export function useEtherscanLink (type, data, networkId) {
   const context = useWeb3Context()
   return useMemo(
     () => getEtherscanLink(networkId || context.networkId, type, data), [networkId, context.networkId, type, data]
   )
 }
 
-export function useAccountEffect(effect, depends) {
+export function useAccountEffect(effect, depends = []) {
   const context = useWeb3Context()
-  useEffect(effect, [...depends, context.account, context.accountReRenderer])
+  useEffect(effect, [...depends, context.networkId, context.account, context.reRenderers.accountReRenderer])
 }
 
-export function useNetworkEffect(effect, depends) {
+export function useNetworkEffect(effect, depends = []) {
   const context = useWeb3Context()
-  useEffect(effect, [...depends, context.networkId, context.networkReRenderer])
+  useEffect(effect, [...depends, context.networkId, context.reRenderers.networkReRenderer])
 }
 
 export function useAccountBalance (address, {numberOfDigits = 3, format} = {}) {
@@ -41,7 +41,7 @@ export function useAccountBalance (address, {numberOfDigits = 3, format} = {}) {
       .then(balance =>
         setBalance(Number(balance).toLocaleString(undefined, { maximumFractionDigits: numberOfDigits }))
       )
-  }, [context.networkId])
+  })
 
   return balance
 }
@@ -55,23 +55,75 @@ export function useERC20Balance (ERC20Address, address, numberOfDigits = 3) {
       .then(balance =>
         setERC20Balance(Number(balance).toLocaleString(undefined, { maximumFractionDigits: numberOfDigits }))
       )
-  }, [context.networkId])
+  })
 
   return ERC20Balance
 }
 
 export function useSignPersonal () {
   const context = useWeb3Context()
-  function wrappedSignPersonal (message) {
+
+  return function wrappedSignPersonal (message) {
     return signPersonal(context.web3js, context.account, message)
   }
-  return wrappedSignPersonal
 }
 
-export function useSendTransaction () {
-  const context = useWeb3Context()
-  function wrappedSendTransaction (method, handlers, transactionOptions) {
-    return sendTransaction(context.web3js, context.account, method, handlers, transactionOptions)
+const initialTransaction = {
+  state: 'ready',
+  data: {
+    transactionHash:          undefined,
+    transactionReceipt:       undefined,
+    transactionConfirmations: undefined,
+    transactionError:         undefined,
+    transactionErrorCode:     undefined
   }
-  return [wrappedSendTransaction, TRANSACTION_ERRORS]
+}
+
+function transactionReducer (state, action) {
+  switch (action.type) {
+    case 'ready':
+      return initialTransaction
+    case 'sending':
+      return { state: 'sending', data: initialTransaction.data }
+    case 'pending':
+      return { state: 'pending', data: { ...state.data, ...action.data } }
+    case 'success':
+      return { state: 'success', data: { ...state.data, ...action.data } }
+    case 'error':
+      return { state: 'error',   data: { ...state.data, ...action.data } }
+    default:
+      return initialTransaction
+  }
+}
+
+export function useTransactionManager (method, { transactionOptions, maximumConfirmations } = {}) {
+  const context = useWeb3Context()
+
+  const [transaction, dispatch] = useReducer(transactionReducer, initialTransaction)
+
+  const handlers = {
+    transactionHash: transactionHash => dispatch({ type: 'pending', data: { transactionHash:    transactionHash } }),
+    receipt: transactionReceipt =>      dispatch({ type: 'success', data: { transactionReceipt: transactionReceipt } }),
+    confirmation: (transactionConfirmations, transactionReceipt) => {
+      if (maximumConfirmations && transactionConfirmations <= maximumConfirmations) {
+        dispatch({
+          type: 'success',
+          data: { transactionConfirmations: transactionConfirmations, transactionReceipt: transactionReceipt }
+        })
+      }
+    }
+  }
+
+  function _sendTransaction () {
+    dispatch({ type: 'sending' })
+    sendTransaction(context.web3js, context.account, method, handlers, transactionOptions)
+      .catch(error => {
+        const transactionErrorCode = TRANSACTION_ERROR_CODES.includes(error.code) ? error.code : undefined
+        dispatch({ type: 'error', data: { transactionError: error, transactionErrorCode: transactionErrorCode } })
+      })
+  }
+
+  function resetTransaction () { dispatch({ type: 'ready' }) }
+
+  return [transaction.state, transaction.data, _sendTransaction, resetTransaction, TRANSACTION_ERROR_CODES]
 }
