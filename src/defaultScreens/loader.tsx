b5831f50a28ecede9563f8426eabeb77ad516db3
Noah Zinsmeister <noahwz@gmail.com>
2019-01-04 17:49:28 -0500

    change loader filename

---
+++ src/defaultScreens/loader.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+import Common, { linkColor } from './common'
+
+function LoadingIcon() {
+  return (
+    <svg width="100%" height="100%" viewBox="0 0 105 105" xmlns="http://www.w3.org/2000/svg" fill={linkColor}>
+      <circle cx="12.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="0s" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="12.5" cy="52.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="100ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="52.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="300ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="52.5" cy="52.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="600ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="92.5" cy="12.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="800ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="92.5" cy="52.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="400ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="12.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="700ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="52.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="500ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+      <circle cx="92.5" cy="92.5" r="12.5">
+        <animate attributeName="fill-opacity"
+          begin="200ms" dur="1s"
+          values="1;.2;1" calcMode="linear"
+          repeatCount="indefinite" />
+      </circle>
+    </svg>
+  )
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+const LoadingWrapper: any = styled.div`
+  width: ${(props: any) => props.width};
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-in;
+  animation-fill-mode: forwards;
+`
+
+export default function Loader () {
+  return (
+    <Common>
+      <LoadingWrapper width='8%'>
+        <LoadingIcon />
+      </LoadingWrapper>
+    </Common>
+  )
+}
