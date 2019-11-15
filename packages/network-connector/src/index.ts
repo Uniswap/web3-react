4d56495f2f9a02ab7dab3968584e9b866cfe8b0f
Noah Zinsmeister <noahwz@gmail.com>
2019-11-15 15:57:23 -0500

    address tree shaking issue with @0x/subproviders

--- packages/network-connector/src/index.ts
+++ packages/network-connector/src/index.ts
@@ -1,6 +1,7 @@
 import { ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
-import { Web3ProviderEngine, RPCSubprovider } from '@0x/subproviders' // use tree-shaking instead of dynamic import
+import Web3ProviderEngine from 'web3-provider-engine'
+import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider' // https://github.com/0xProject/0x-monorepo/issues/1400
 
 export interface NetworkConnectorArguments {
   url: string
