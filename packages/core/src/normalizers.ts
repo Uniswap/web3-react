5a12c6f47e5b6da5466fcd0cdb40208107096be8
Pierre Bertet <hello@pierre.world>
2020-01-31 16:08:43 +0000

    MetaMask Mobile compatibility fix

--- packages/core/src/normalizers.ts
+++ packages/core/src/normalizers.ts
@@ -4,6 +4,11 @@ import invariant from 'tiny-invariant'
 
 export function normalizeChainId(chainId: string | number): number {
   if (typeof chainId === 'string') {
+    // Temporary fix until the next version of Metamask Mobile gets released.
+    // In the current version (0.2.13), the chainId starts with “Ox” rather
+    // than “0x”. Fix: https://github.com/MetaMask/metamask-mobile/pull/1275
+    chainId = chainId.replace(/^Ox/, '0x')
+
     const parsedChainId = Number.parseInt(chainId, chainId.trim().substring(0, 2) === '0x' ? 16 : 10)
     invariant(!Number.isNaN(parsedChainId), `chainId ${chainId} is not an integer`)
     return parsedChainId
