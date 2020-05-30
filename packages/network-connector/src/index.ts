42839f6a3f96121c0e763ac714fb550cfaec4139
Moody Salem <moody.salem@gmail.com>
2020-05-29 22:01:23 -0400

    bye bye web3 cruft from network provider

--- packages/network-connector/src/index.ts
+++ packages/network-connector/src/index.ts
@@ -1,47 +1,90 @@
 import { ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
-import Web3ProviderEngine from 'web3-provider-engine'
-import CacheSubprovider from 'web3-provider-engine/subproviders/cache.js'
-import { RPCSubprovider } from '@0x/subproviders/lib/src/subproviders/rpc_subprovider' // https://github.com/0xProject/0x-monorepo/issues/1400
 import invariant from 'tiny-invariant'
 
 interface NetworkConnectorArguments {
   urls: { [chainId: number]: string }
   defaultChainId?: number
-  pollingInterval?: number
-  requestTimeoutMs?: number
+}
+
+// taken from ethers.js, compatible interface with web3 provider
+type AsyncSendable = {
+  isMetaMask?: boolean;
+  host?: string;
+  path?: string;
+  sendAsync?: (request: any, callback: (error: any, response: any) => void) => void;
+  send?: (request: any, callback: (error: any, response: any) => void) => void;
+}
+
+class MiniRpcProvider implements AsyncSendable {
+  public readonly isMetaMask: false = false
+  public readonly chainId: number
+  public readonly url: string
+  public readonly host: string
+  public readonly path: string
+
+  constructor(chainId: number, url: string) {
+    this.chainId = chainId
+    this.url = url
+    const parsed = new URL(url)
+    this.host = parsed.host
+    this.path = parsed.pathname
+  }
+
+  sendAsync(request: any, callback: (error: any, response: any) => void): void {
+    fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(request)
+    })
+      .then(
+        result => {
+          return result.json()
+            .then(
+              json => {
+                if (!result.ok) {
+                  throw new Error(`${json.code}: ${json.message}`)
+                }
+                return json
+              }
+            )
+        }
+      )
+      .then(
+        json => {
+          callback(undefined, json)
+        }
+      )
+      .catch(
+        error => {
+          callback(error, undefined)
+        }
+      )
+  }
 }
 
 export class NetworkConnector extends AbstractConnector {
-  private readonly providers: { [chainId: number]: any }
+  private readonly providers: { [chainId: number]: MiniRpcProvider }
   private currentChainId: number
-  private readonly pollingInterval?: number
-  private readonly requestTimeoutMs?: number
   private active: boolean
 
-  constructor({ urls, defaultChainId, pollingInterval, requestTimeoutMs }: NetworkConnectorArguments) {
+  constructor({ urls, defaultChainId }: NetworkConnectorArguments) {
     invariant(defaultChainId || Object.keys(urls).length === 1, 'defaultChainId is a required argument with >1 url')
     super({ supportedChainIds: Object.keys(urls).map((k): number => Number(k)) })
 
     this.currentChainId = defaultChainId || Number(Object.keys(urls)[0])
-    this.pollingInterval = pollingInterval
-    this.requestTimeoutMs = requestTimeoutMs
-    this.providers = Object.keys(urls).reduce((accumulator, chainId) => {
-      const engine = new Web3ProviderEngine({ pollingInterval: this.pollingInterval })
-      engine.addProvider(new CacheSubprovider())
-      engine.addProvider(new RPCSubprovider(urls[Number(chainId)], this.requestTimeoutMs))
-      return Object.assign(accumulator, { [Number(chainId)]: engine })
+    this.providers = Object.keys(urls).reduce<{ [chainId: number]: MiniRpcProvider }>((accumulator, chainId) => {
+      accumulator[Number(chainId)] = new MiniRpcProvider(Number(chainId), urls[Number(chainId)])
+      return accumulator
     }, {})
     this.active = false
   }
 
   public async activate(): Promise<ConnectorUpdate> {
-    this.providers[this.currentChainId].start()
     this.active = true
     return { provider: this.providers[this.currentChainId], chainId: this.currentChainId, account: null }
   }
 
-  public async getProvider(): Promise<Web3ProviderEngine> {
+  public async getProvider(): Promise<MiniRpcProvider> {
     return this.providers[this.currentChainId]
   }
 
@@ -54,16 +97,13 @@ export class NetworkConnector extends AbstractConnector {
   }
 
   public deactivate() {
-    this.providers[this.currentChainId].stop()
     this.active = false
   }
 
   public changeChainId(chainId: number) {
     invariant(Object.keys(this.providers).includes(chainId.toString()), `No url found for chainId ${chainId}`)
     if (this.active) {
-      this.providers[this.currentChainId].stop()
       this.currentChainId = chainId
-      this.providers[this.currentChainId].start()
       this.emitUpdate({ provider: this.providers[this.currentChainId], chainId })
     } else {
       this.currentChainId = chainId
