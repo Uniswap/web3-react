392fa5fa58cadc1637ef6449086179be0deef3d6
Alex Miller <asmiller1989@gmail.com>
2021-01-13 13:53:32 -0600

    Adds lattice-connector to README and example app

--- example/connectors.ts
+++ example/connectors.ts
@@ -4,6 +4,7 @@ import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { LedgerConnector } from '@web3-react/ledger-connector'
 import { TrezorConnector } from '@web3-react/trezor-connector'
+import { LatticeConnector } from '@web3-react/lattice-connector'
 import { FrameConnector } from '@web3-react/frame-connector'
 import { AuthereumConnector } from '@web3-react/authereum-connector'
 import { FortmaticConnector } from '@web3-react/fortmatic-connector'
@@ -44,6 +45,12 @@ export const trezor = new TrezorConnector({
   pollingInterval: POLLING_INTERVAL,
   manifestEmail: 'dummy@abc.xyz',
   manifestAppUrl: 'http://localhost:1234'
+})
+
+export const lattice = new LatticeConnector({
+  chainId: 4,
+  appName: 'web3-react',
+  url: RPC_URLS[4],
 })
 
 export const frame = new FrameConnector({ supportedChainIds: [1] })
