0c6efefdeaa502a948fce318f617246c2b0d3b7c
Noah Zinsmeister <noahwz@gmail.com>
2021-09-29 12:02:37 -0400

    remove frame for now

--- packages/example/connectors/frame.ts
+++
@@ -1,4 +0,0 @@
-import { initializeConnector } from '@web3-react/core'
-import { Frame } from '@web3-react/frame'
-
-export const [frame, useFrame] = initializeConnector<Frame>(Frame)
--- packages/example/connectors/index.ts
+++ packages/example/connectors/index.ts
@@ -3,11 +3,9 @@ import { Connector, Web3ReactState } from '@web3-react/types'
 import { network, useNetwork } from './network'
 import { metaMask, useMetaMask } from './metaMask'
 import { walletConnect, useWalletConnect } from './walletConnect'
-import { frame, useFrame } from './frame'
 
 export const connectors: [Connector, UseStore<Web3ReactState>][] = [
   [network, useNetwork],
   [metaMask, useMetaMask],
   [walletConnect, useWalletConnect],
-  [frame, useFrame],
 ]
