499765d12d382c4f73409987ed0c1294ff0ed8e3
Noah Zinsmeister <noahwz@gmail.com>
2021-10-01 14:03:54 -0400

    type next.config.js

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   webpack5: false,
   env: {
     infuraKey: '84842078b09946638c03157f83405213',
     alchemyKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
   },
 }
+
+module.exports = nextConfig
