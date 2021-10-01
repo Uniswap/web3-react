d707d193a4c1622c9c1cc8904c8eeae5a294cf01
v 1 r t l <pilll.PL22@gmail.com>
2021-10-01 22:45:49 +0300

    add WalletLink connector (#284)
    * feat(connectors): wip WalletLink connector
    
    * feat(connectors): add `WalletLink` to example
    
    * Update packages/walletlink/package.json
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>
    
    * make walletlink a peer dep
    
    * import walletlink like WC
    
    * fix import
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

--- packages/example/connectors/index.ts
+++ packages/example/connectors/index.ts
@@ -3,9 +3,11 @@ import { Connector, Web3ReactState } from '@web3-react/types'
 import { network, useNetwork } from './network'
 import { metaMask, useMetaMask } from './metaMask'
 import { walletConnect, useWalletConnect } from './walletConnect'
+import { walletLink, useWalletLink } from './walletLink'
 
 export const connectors: [Connector, UseStore<Web3ReactState>][] = [
   [network, useNetwork],
   [metaMask, useMetaMask],
   [walletConnect, useWalletConnect],
+  [walletLink, useWalletLink],
 ]
---
+++ packages/walletlink/src/index.ts
@@ -0,0 +1,96 @@
+import { Connector, Actions, Provider } from '@web3-react/types'
+import type { WalletLinkOptions } from 'walletlink/dist/WalletLink'
+
+export class NoWalletLinkError extends Error {
+  public constructor() {
+    super('WalletLink not installed')
+    this.name = NoWalletLinkError.name
+    Object.setPrototypeOf(this, NoWalletLinkError.prototype)
+  }
+}
+
+function parseChainId(chainId: string) {
+  return Number.parseInt(chainId, 16)
+}
+
+interface WalletLinkConnectorArguments extends WalletLinkOptions {
+  url: string
+}
+
+export class WalletLink extends Connector {
+  private readonly options: WalletLinkConnectorArguments
+  private providerPromise?: Promise<void>
+
+  constructor(actions: Actions, options: WalletLinkConnectorArguments, connectEagerly = true) {
+    super(actions)
+    this.options = options
+
+    if (connectEagerly) {
+      this.providerPromise = this.startListening(connectEagerly)
+    }
+  }
+
+  private async startListening(connectEagerly: boolean): Promise<void> {
+    const { url, ...options } = this.options
+
+    const provider = await import('walletlink')
+      .then((m) => m?.default ?? m)
+      .then((WalletLink) => new WalletLink(options).makeWeb3Provider(url))
+
+    this.provider = (provider as unknown as Provider) ?? undefined
+
+    if (this.provider) {
+      this.provider.on('connect', ({ chainId }: { chainId: string }): void => {
+        this.actions.update({ chainId: parseChainId(chainId) })
+      })
+      this.provider.on('disconnect', (error: Error): void => {
+        this.actions.reportError(error)
+      })
+      this.provider.on('chainChanged', (chainId: string): void => {
+        this.actions.update({ chainId: parseChainId(chainId) })
+      })
+      this.provider.on('accountsChanged', (accounts: string[]): void => {
+        this.actions.update({ accounts })
+      })
+
+      if (connectEagerly) {
+        return Promise.all([
+          this.provider.request({ method: 'eth_chainId' }) as Promise<string>,
+          this.provider.request({ method: 'eth_accounts' }) as Promise<string[]>,
+        ])
+          .then(([chainId, accounts]) => {
+            if (accounts.length > 0) {
+              this.actions.update({ chainId: parseChainId(chainId), accounts })
+            }
+          })
+          .catch((error) => {
+            console.debug('Could not connect eagerly', error)
+          })
+      }
+    }
+  }
+
+  public async activate(): Promise<void> {
+    this.actions.startActivation()
+
+    if (!this.providerPromise) {
+      this.providerPromise = this.startListening(false)
+    }
+    await this.providerPromise
+
+    if (this.provider) {
+      await Promise.all([
+        this.provider.request({ method: 'eth_chainId' }) as Promise<string>,
+        this.provider.request({ method: 'eth_requestAccounts' }) as Promise<string[]>,
+      ])
+        .then(([chainId, accounts]) => {
+          this.actions.update({ chainId: Number.parseInt(chainId, 16), accounts })
+        })
+        .catch((error) => {
+          this.actions.reportError(error)
+        })
+    } else {
+      this.actions.reportError(new NoWalletLinkError())
+    }
+  }
+}
