4ada22d85b5cf4e9f21c167faf3676e53faf8849
Noah Zinsmeister <noahwz@gmail.com>
2021-10-01 13:20:41 -0400

    try some esm changes

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -2,10 +2,17 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  webpack5: false,
+  experimental: { esmExternals: true },
   env: {
     infuraKey: '84842078b09946638c03157f83405213',
     alchemyKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      events: require.resolve('events'),
+    }
+    return config
   },
 }
 
