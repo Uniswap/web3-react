d97be64167ee80725887a5bff187dd14c79ede57
Noah Zinsmeister <noahwz@gmail.com>
2021-10-11 17:39:32 +0200

    clean up webpack config

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -2,12 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  webpack5: true,
   env: {
     infuraKey: '84842078b09946638c03157f83405213',
     alchemyKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
   },
-  esmExternals: 'loose',
+  esmExternals: true,
   webpack: (config) => {
     config.resolve.fallback = { events: require.resolve('events/'), process: require.resolve('process/browser') }
     return config
