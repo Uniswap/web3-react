93bd94b08a3d111069ee765e168bf70e129fb767
Noah Zinsmeister <noahwz@gmail.com>
2022-01-04 12:32:00 -0500

    export singleton EMPTY

--- packages/empty/src/index.ts
+++ packages/empty/src/index.ts
@@ -1,11 +1,17 @@
 import type { Actions } from '@web3-react/types'
 import { Connector } from '@web3-react/types'
 
-export class Empty extends Connector {
+class Empty extends Connector {
+  provider: undefined
+
   constructor(actions: Actions) {
     super(actions)
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  public async activate(): Promise<void> {}
+  public activate() {}
 }
+
+// @ts-expect-error this is okay because actions aren't ever validated,
+// and they're only used to set a protected property
+export const EMPTY = new Empty()
