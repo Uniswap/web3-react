97c1fc2f38d6ffaeec8463e5a524dd48977010ec
Noah Zinsmeister <noahwz@gmail.com>
2022-01-07 15:09:25 -0500

    add tests for walletconnect + walletlink

---
+++ packages/walletconnect/src/index.spec.ts
@@ -0,0 +1,55 @@
+import { createWeb3ReactStoreAndActions } from '@web3-react/store'
+import type { Actions, RequestArguments, Web3ReactStore } from '@web3-react/types'
+import { WalletConnect } from '.'
+import { MockEIP1193Provider } from '../../eip1193/src/index.spec'
+
+// necessary because walletconnect returns chainId as a number
+export class MockMockWalletConnectProvider extends MockEIP1193Provider {
+  public eth_chainId_number = jest.fn((chainId?: string) =>
+    chainId === undefined ? chainId : Number.parseInt(chainId, 16)
+  )
+
+  public request(x: RequestArguments): Promise<unknown> {
+    if (x.method === 'eth_chainId') {
+      return Promise.resolve(this.eth_chainId_number(this.chainId))
+    } else {
+      return super.request(x)
+    }
+  }
+}
+
+jest.mock('@walletconnect/ethereum-provider', () => MockMockWalletConnectProvider)
+
+const chainId = '0x1'
+const accounts: string[] = []
+
+describe('WalletConnect', () => {
+  let store: Web3ReactStore
+  let connector: WalletConnect
+  let mockConnector: MockMockWalletConnectProvider
+
+  describe('connectEagerly = true', () => {
+    beforeEach(() => {
+      let actions: Actions
+      ;[store, actions] = createWeb3ReactStoreAndActions()
+      connector = new WalletConnect(actions, {})
+    })
+
+    beforeEach(async () => {
+      mockConnector = connector.provider as unknown as MockMockWalletConnectProvider
+      mockConnector.chainId = chainId
+      mockConnector.accounts = accounts
+    })
+
+    test('#activate', async () => {
+      await connector.activate()
+
+      expect(store.getState()).toEqual({
+        chainId: Number.parseInt(chainId, 16),
+        accounts,
+        activating: false,
+        error: undefined,
+      })
+    })
+  })
+})
---
+++ packages/walletlink/src/index.spec.ts
@@ -0,0 +1,49 @@
+import { createWeb3ReactStoreAndActions } from '@web3-react/store'
+import type { Actions, Web3ReactStore } from '@web3-react/types'
+import { WalletLink } from '.'
+import { MockEIP1193Provider } from '../../eip1193/src/index.spec'
+
+jest.mock('walletlink', () => ({
+  WalletLink: class MockWalletLink {
+    makeWeb3Provider() {
+      return new MockEIP1193Provider()
+    }
+  },
+}))
+
+const chainId = '0x1'
+const accounts: string[] = []
+
+describe('WalletLink', () => {
+  let store: Web3ReactStore
+  let connector: WalletLink
+  let mockConnector: MockEIP1193Provider
+
+  describe('connectEagerly = true', () => {
+    beforeEach(() => {
+      let actions: Actions
+      ;[store, actions] = createWeb3ReactStoreAndActions()
+      connector = new WalletLink(actions, {
+        appName: 'test',
+        url: 'https://mock.url',
+      })
+    })
+
+    beforeEach(async () => {
+      mockConnector = connector.provider as unknown as MockEIP1193Provider
+      mockConnector.chainId = chainId
+      mockConnector.accounts = accounts
+    })
+
+    test('#activate', async () => {
+      await connector.activate()
+
+      expect(store.getState()).toEqual({
+        chainId: Number.parseInt(chainId, 16),
+        accounts,
+        activating: false,
+        error: undefined,
+      })
+    })
+  })
+})
