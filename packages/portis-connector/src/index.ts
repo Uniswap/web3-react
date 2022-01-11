117730f062de482c39c0c87112cc5f59bbff0147
Noah Zinsmeister <noahwz@gmail.com>
2022-01-11 15:21:22 -0500

    add networks

--- packages/portis-connector/src/index.ts
+++ packages/portis-connector/src/index.ts
@@ -1,5 +1,5 @@
-import { ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
+import { ConnectorUpdate } from '@web3-react/types'
 import invariant from 'tiny-invariant'
 
 const chainIdToNetwork: { [network: number]: string } = {
@@ -21,7 +21,8 @@ const chainIdToNetwork: { [network: number]: string } = {
   122: 'fuse',
   1337: 'devChain',
   31337: 'hardhat',
-  15001: 'maticTestnet'
+  137: 'matic',
+  80001: 'maticMumbai'
 }
 
 type Network = number | { chainId: string; [key: string]: any }
