f42f57fbe8d17f2577ba9c5a15fc2acd8a13213d
Noah Zinsmeister <noahwz@gmail.com>
2022-01-13 17:50:54 -0500

    get example working
    bump @walletconnect/types
    
    fix prettier

--- example/connectors.ts
+++ example/connectors.ts
@@ -1,28 +1,28 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { NetworkConnector } from '@web3-react/network-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { WalletLinkConnector } from '@web3-react/walletlink-connector'
-import { LedgerConnector } from '@web3-react/ledger-connector'
-import { TrezorConnector } from '@web3-react/trezor-connector'
-import { LatticeConnector } from '@web3-react/lattice-connector'
-import { FrameConnector } from '@web3-react/frame-connector'
 import { AuthereumConnector } from '@web3-react/authereum-connector'
 import { FortmaticConnector } from '@web3-react/fortmatic-connector'
+import { FrameConnector } from '@web3-react/frame-connector'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { LatticeConnector } from '@web3-react/lattice-connector'
+import { LedgerConnector } from '@web3-react/ledger-connector'
 import { MagicConnector } from '@web3-react/magic-connector'
+import { NetworkConnector } from '@web3-react/network-connector'
 import { PortisConnector } from '@web3-react/portis-connector'
 import { TorusConnector } from '@web3-react/torus-connector'
+import { TrezorConnector } from '@web3-react/trezor-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS: { [chainId: number]: string } = {
   1: process.env.RPC_URL_1 as string,
-  4: process.env.RPC_URL_4 as string,
+  4: process.env.RPC_URL_4 as string
 }
 
 export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
 
 export const network = new NetworkConnector({
   urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
-  defaultChainId: 1,
+  defaultChainId: 1
 })
 
 export const walletconnect = new WalletConnectConnector({
@@ -30,13 +30,13 @@ export const walletconnect = new WalletConnectConnector({
   chainId: 1,
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
-  pollingInterval: POLLING_INTERVAL,
+  pollingInterval: POLLING_INTERVAL
 })
 
 export const walletlink = new WalletLinkConnector({
   url: RPC_URLS[1],
   appName: 'web3-react example',
-  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001],
+  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001]
 })
 
 export const ledger = new LedgerConnector({ chainId: 1, url: RPC_URLS[1], pollingInterval: POLLING_INTERVAL })
@@ -46,13 +46,13 @@ export const trezor = new TrezorConnector({
   url: RPC_URLS[1],
   pollingInterval: POLLING_INTERVAL,
   manifestEmail: 'dummy@abc.xyz',
-  manifestAppUrl: 'http://localhost:1234',
+  manifestAppUrl: 'http://localhost:1234'
 })
 
 export const lattice = new LatticeConnector({
   chainId: 4,
   appName: 'web3-react',
-  url: RPC_URLS[4],
+  url: RPC_URLS[4]
 })
 
 export const frame = new FrameConnector({ supportedChainIds: [1] })
@@ -64,7 +64,7 @@ export const fortmatic = new FortmaticConnector({ apiKey: process.env.FORTMATIC_
 export const magic = new MagicConnector({
   apiKey: process.env.MAGIC_API_KEY as string,
   chainId: 4,
-  email: 'hello@example.org',
+  email: 'hello@example.org'
 })
 
 export const portis = new PortisConnector({ dAppId: process.env.PORTIS_DAPP_ID as string, networks: [1, 100] })
--- example/next.config.js
+++ example/next.config.js
@@ -7,9 +7,8 @@ const config = {
     RPC_URL_4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
     FORTMATIC_API_KEY: 'pk_test_A6260FCBAA2EBDFB',
     MAGIC_API_KEY: 'pk_test_398B82F5F0E88874',
-    PORTIS_DAPP_ID: 'e9be171c-2b7f-4ff0-8db9-327707511ee2',
+    PORTIS_DAPP_ID: 'e9be171c-2b7f-4ff0-8db9-327707511ee2'
   },
-
   webpack5: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -17,10 +16,21 @@ const config = {
         ...config.resolve.fallback,
         net: false,
         tls: false,
+        electron: false,
+        '@ethereumjs/tx': false
+      }
+    } else {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        electron: false,
+        '@ethereumjs/tx': false
       }
     }
     return config
   },
+  experimental: {
+    esmExternals: 'loose'
+  }
 }
 
 module.exports = config
