e81fcec6c738099d6ec21b6fd53946fc9af4fde5
Noah Zinsmeister <noahwz@gmail.com>
2022-01-25 15:51:44 -0500

    clean up example

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -7,17 +7,6 @@ const nextConfig = {
     alchemyKey: process.env.ALCHEMY_KEY,
     magicKey: process.env.MAGIC_KEY,
   },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      events: require.resolve('events/'),
-      process: require.resolve('process/browser'),
-      buffer: require.resolve('buffer/'),
-      util: require.resolve('util/'),
-      bufferutil: false,
-      'utf-8-validate': false,
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
