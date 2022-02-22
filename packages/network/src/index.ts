321e024d9a73d79111505b6c238affb47bea2966
Noah Zinsmeister <noahwz@gmail.com>
2022-02-22 17:22:26 -0500

    add connectEagerly to top 4 connectors

--- packages/network/src/index.ts
+++ packages/network/src/index.ts
@@ -7,82 +7,52 @@ type url = string | ConnectionInfo
 
 export class Network extends Connector {
   /** {@inheritdoc Connector.provider} */
-  provider: Eip1193Bridge | undefined
+  public provider: Eip1193Bridge | undefined
 
-  private urlMap: { [chainId: number]: url[] }
-  private chainId: number
-  private providerCache: { [chainId: number]: Eip1193Bridge } = {}
+  private urlMap: Record<number, url[]>
+  private desiredChainId: number
+  private providerCache: Partial<Record<number, Promise<Eip1193Bridge>>> = {}
 
   /**
    * @param urlMap - A mapping from chainIds to RPC urls.
    * @param connectEagerly - A flag indicating whether connection should be initiated when the class is constructed.
    */
-  constructor(actions: Actions, urlMap: { [chainId: number]: url | url[] }, connectEagerly = true) {
+  constructor(actions: Actions, urlMap: { [chainId: number]: url | url[] }, connectEagerly = false) {
     super(actions)
+
+    if (connectEagerly && typeof window === 'undefined') {
+      throw new Error('connectEagerly = true is invalid for SSR, instead use the connectEagerly method in a useEffect')
+    }
+
     this.urlMap = Object.keys(urlMap).reduce<{ [chainId: number]: url[] }>((accumulator, chainId) => {
       const urls = urlMap[Number(chainId)]
       accumulator[Number(chainId)] = Array.isArray(urls) ? urls : [urls]
       return accumulator
     }, {})
-    // use the first chainId in urlMap as the default
-    this.chainId = Number(Object.keys(this.urlMap)[0])
+    this.desiredChainId = Number(Object.keys(this.urlMap)[0])
 
-    if (connectEagerly) {
-      void this.initialize()
-    }
+    if (connectEagerly) void this.activate()
   }
 
-  private async initialize(): Promise<void> {
-    this.provider = undefined
-    this.actions.startActivation()
+  private async isomorphicInitialize(chainId: number) {
+    if (this.providerCache[chainId]) return await this.providerCache[chainId]
 
-    // cache the desired chainId before async logic
-    const chainId = this.chainId
-
-    // populate the provider cache if necessary
-    if (!this.providerCache[chainId]) {
-      // instantiate new provider
-      const [{ JsonRpcProvider, FallbackProvider }, Eip1193Bridge] = await Promise.all([
-        import('@ethersproject/providers').then(({ JsonRpcProvider, FallbackProvider }) => ({
-          JsonRpcProvider,
-          FallbackProvider,
-        })),
-        import('@ethersproject/experimental').then(({ Eip1193Bridge }) => Eip1193Bridge),
-      ])
-
+    await (this.providerCache[chainId] = Promise.all([
+      import('@ethersproject/providers').then(({ JsonRpcProvider, FallbackProvider }) => ({
+        JsonRpcProvider,
+        FallbackProvider,
+      })),
+      import('@ethersproject/experimental').then(({ Eip1193Bridge }) => Eip1193Bridge),
+    ]).then(([{ JsonRpcProvider, FallbackProvider }, Eip1193Bridge]) => {
       const urls = this.urlMap[chainId]
 
       const providers = urls.map((url) => new JsonRpcProvider(url, chainId))
-      const provider = new Eip1193Bridge(
+
+      return new Eip1193Bridge(
         providers[0].getSigner(),
         providers.length === 1 ? providers[0] : new FallbackProvider(providers)
       )
-
-      this.providerCache[chainId] = provider
-    }
-
-    // once we're here, the cache is guaranteed to be initialized
-    // so, if the current chainId still matches the one at the beginning of the call, update
-    if (chainId === this.chainId) {
-      this.provider = this.providerCache[chainId]
-
-      return this.provider
-        .request({ method: 'eth_chainId' })
-        .then((returnedChainId: number) => {
-          if (returnedChainId !== chainId) {
-            // this means the returned chainId was unexpected, i.e. the provided url(s) were wrong
-            throw new Error(`expected chainId ${chainId}, received ${returnedChainId}`)
-          }
-
-          // again we have to make sure the chainIds match, to prevent race conditions
-          if (chainId === this.chainId) {
-            this.actions.update({ chainId, accounts: [] })
-          }
-        })
-        .catch((error: Error) => {
-          this.actions.reportError(error)
-        })
-    }
+    }))
   }
 
   /**
@@ -90,14 +60,29 @@ export class Network extends Connector {
    *
    * @param desiredChainId - The desired chain to connect to.
    */
-  public async activate(desiredChainId = Number(Object.keys(this.urlMap)[0])): Promise<void> {
-    if (this.urlMap[desiredChainId] === undefined) {
-      throw new Error(`no url(s) provided for desiredChainId ${desiredChainId}`)
-    }
+  public async activate(desiredChainId = this.desiredChainId): Promise<void> {
+    const cancelActivation = this.actions.startActivation()
 
-    // set the connector's chainId to the target, to prevent race conditions
-    this.chainId = desiredChainId
+    this.desiredChainId = desiredChainId
+    await this.isomorphicInitialize(desiredChainId)
+    const provider = await this.providerCache[desiredChainId]
 
-    return this.initialize()
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return provider!
+      .request({ method: 'eth_chainId' })
+      .then((returnedChainId: number) => {
+        // ensure the returned chainId is as expected, i.e. the provided url(s) are correct
+        if (returnedChainId !== desiredChainId)
+          throw new Error(`expected chainId ${desiredChainId}, received ${returnedChainId}`)
+
+        if (this.desiredChainId === desiredChainId) {
+          this.actions.update({ chainId: desiredChainId, accounts: [] })
+        } else {
+          cancelActivation()
+        }
+      })
+      .catch((error: Error) => {
+        this.actions.reportError(error)
+      })
   }
 }
