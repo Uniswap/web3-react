7e3c85137b3fce2faf803f4aa003537034a7ac79
Alex Gogl <alex@gogl.io>
2022-03-26 13:00:55 +0100

    upgrade nextra and add some more pages

--- packages/docs/pages/_app.tsx
+++ packages/docs/pages/_app.tsx
@@ -1,14 +1,12 @@
-import type { AppProps } from 'next/app'
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import '../styles/globals.css'
 import 'nextra-theme-docs/style.css'
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
+export default function Nextra({ Component, pageProps }: { Component: any, pageProps: any }) {
+  const getLayout = Component.getLayout || ((page: any) => page)
+  return getLayout(<Component {...pageProps} />)
 }
-
-export default App
