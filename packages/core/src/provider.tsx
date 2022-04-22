8928e378e53be0f7f9160ac79cac7efe7b1e2e7f
Noah Zinsmeister <noahwz@gmail.com>
2022-04-22 16:50:50 -0400

    first pass

--- packages/core/src/provider.tsx
+++ packages/core/src/provider.tsx
@@ -1,12 +1,12 @@
 import type { Networkish } from '@ethersproject/networks'
 import type { BaseProvider, Web3Provider } from '@ethersproject/providers'
 import type { Connector } from '@web3-react/types'
-import type { ReactNode } from 'react'
+import type { Context, ReactNode } from 'react'
 import React, { createContext, useContext } from 'react'
 import type { Web3ReactHooks, Web3ReactPriorityHooks } from './hooks'
 import { getPriorityConnector } from './hooks'
 
-type Web3ContextType = {
+type Web3ContextType<T extends BaseProvider = Web3Provider> = {
   connector: ReturnType<Web3ReactPriorityHooks['usePriorityConnector']>
   chainId: ReturnType<Web3ReactPriorityHooks['usePriorityChainId']>
   accounts: ReturnType<Web3ReactPriorityHooks['usePriorityAccounts']>
@@ -14,14 +14,14 @@ type Web3ContextType = {
   error: ReturnType<Web3ReactPriorityHooks['usePriorityError']>
   account: ReturnType<Web3ReactPriorityHooks['usePriorityAccount']>
   isActive: ReturnType<Web3ReactPriorityHooks['usePriorityIsActive']>
-  provider: ReturnType<Web3ReactPriorityHooks['usePriorityProvider']>
+  provider: Web3Provider | undefined | T
   ENSNames: ReturnType<Web3ReactPriorityHooks['usePriorityENSNames']>
   ENSName: ReturnType<Web3ReactPriorityHooks['usePriorityENSName']>
 }
 
 const Web3Context = createContext<Web3ContextType | undefined>(undefined)
 
-export function Web3ReactProvider<T extends BaseProvider = Web3Provider>({
+export function Web3ReactProvider({
   children,
   connectors,
   network,
@@ -52,7 +52,12 @@ export function Web3ReactProvider<T extends BaseProvider = Web3Provider>({
   const error = usePriorityError()
   const account = usePriorityAccount()
   const isActive = usePriorityIsActive()
-  const provider = usePriorityProvider<T>(network)
+  // note that we've omitted a <T extends BaseProvider = Web3Provider> generic type
+  // in Web3ReactProvider, and thus can't pass T through to usePriorityProvider below.
+  // this is because if we did so, the type of provider would include T, but that would
+  // conflict because Web3Context can't take a generic. as a work-around,
+  // we manually type useWeb3React below
+  const provider = usePriorityProvider(network)
   const ENSNames = usePriorityENSNames(lookupENS ? provider : undefined)
   const ENSName = usePriorityENSName(lookupENS ? provider : undefined)
 
@@ -76,8 +81,11 @@ export function Web3ReactProvider<T extends BaseProvider = Web3Provider>({
   )
 }
 
-export function useWeb3React() {
-  const web3 = useContext(Web3Context)
+export function useWeb3React<T extends BaseProvider = Web3Provider>() {
+  // see the comment in Web3ReactProvider for an explanation of the below
+  const web3 = useContext<Web3ContextType<T> | undefined>(
+    Web3Context as unknown as Context<Web3ContextType<T> | undefined>
+  )
   if (!web3) throw Error('useWeb3React can only be used within the Web3ReactProvider component')
   return web3
 }
