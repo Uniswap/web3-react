e1b13dfbfb19582e060368135b02b5ce88f5b1c1
Vignesh Mohankumar <me@vig.xyz>
2022-06-06 17:52:27 -0400

    network fix

--- packages/example-next/connectors/network.ts
+++ packages/example-next/connectors/network.ts
@@ -2,4 +2,4 @@ import { initializeConnector } from '@web3-react/core'
 import { Network } from '@web3-react/network'
 import { URLS } from '../chains'
 
-export const [network, hooks] = initializeConnector<Network>((actions) => new Network(actions, URLS))
+export const [network, hooks] = initializeConnector<Network>((actions) => new Network({ actions, urlMap: URLS }))
