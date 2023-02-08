2c0b5fbe47931e8ecffa32adcc8cd1dbaf1d6637
Mike <grabbou@gmail.com>
2023-02-08 22:03:37 +0400

    chore: this is in fact, not needed

--- example/components/ConnectWithSelect.tsx
+++ example/components/ConnectWithSelect.tsx
@@ -136,7 +136,7 @@ export function ConnectWithSelect({
         <div style={{ marginBottom: '1rem' }} />
         <button
           onClick={() => {
-            if ('deactivate' in connector) {
+            if (connector?.deactivate) {
               void connector.deactivate()
             } else {
               void connector.resetState()
