4200dbe4d9d90a356edbe144e7eb2baccecbee25
Mike <grabbou@gmail.com>
2023-02-16 00:55:41 +0400

    temporary while this is fixed

--- example/components/Card.tsx
+++ example/components/Card.tsx
@@ -50,6 +50,7 @@ export function Card({
         borderRadius: '1rem',
       }}
     >
+      {/* @ts-ignore @todo walletconnect is not EIP-1193 compliant yet. Fix on the way */}
       <b>{getName(connector)}</b>
       <div style={{ marginBottom: '1rem' }}>
         <Status isActivating={isActivating} isActive={isActive} error={error} />
