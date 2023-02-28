5f183497fd03cf58c7f40e7e7ab1912ea6a3e366
Mike <grabbou@gmail.com>
2023-02-28 20:43:42 +0400

    fix

--- example/components/Card.tsx
+++ example/components/Card.tsx
@@ -3,7 +3,7 @@ import type { Web3ReactHooks } from '@web3-react/core'
 import type { GnosisSafe } from '@web3-react/gnosis-safe'
 import type { MetaMask } from '@web3-react/metamask'
 import type { Network } from '@web3-react/network'
-import type { WalletConnect } from '@web3-react/walletconnect-v2'
+import type { WalletConnect } from '@web3-react/walletconnect'
 import type { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
 
 import { getName } from '../utils'
