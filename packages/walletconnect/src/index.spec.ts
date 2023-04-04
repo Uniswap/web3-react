615494c285c70a6c560f68fcdfe6e9813c1e67fe
Mike Grabowski <grabbou@gmail.com>
2023-04-05 01:37:12 +0400

    feat: replace node:events with eventemitter3 (#792)

--- packages/walletconnect/src/index.spec.ts
+++ packages/walletconnect/src/index.spec.ts
@@ -1,6 +1,6 @@
 import { createWeb3ReactStoreAndActions } from '@web3-react/store'
 import type { Actions, RequestArguments, Web3ReactStore } from '@web3-react/types'
-import EventEmitter from 'node:events'
+import EventEmitter from 'eventemitter3'
 import { WalletConnect } from '.'
 import { MockEIP1193Provider } from '../../eip1193/src/mock'
 
