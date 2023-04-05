3127f9a222267c04121bc8042cd9ac2a08c15de3
Mike Grabowski <grabbou@gmail.com>
2023-04-05 21:05:32 +0400

    chore: add implements (#794)

--- packages/eip1193/src/index.spec.ts
+++ packages/eip1193/src/index.spec.ts
@@ -2,10 +2,10 @@ import { Eip1193Bridge } from '@ethersproject/experimental'
 import { Web3Provider } from '@ethersproject/providers'
 import { createWeb3ReactStoreAndActions } from '@web3-react/store'
 import { MockEIP1193Provider } from '@web3-react/core'
-import type { Actions, Web3ReactStore } from '@web3-react/types'
+import type { Actions, Web3ReactStore, ProviderRpcError } from '@web3-react/types'
 import { EIP1193 } from '.'
 
-class MockProviderRpcError extends Error {
+class MockProviderRpcError extends Error implements ProviderRpcError {
   public code: number
   constructor() {
     super('Mock Provider RPC Error')
