859e04bab85bbcb805a666ec51890014ba8c07c9
Mike <grabbou@gmail.com>
2023-04-20 20:18:59 +0400

    feat: update unit tests

--- packages/walletconnect-v2/src/utils.spec.ts
+++ packages/walletconnect-v2/src/utils.spec.ts
@@ -39,37 +39,37 @@ jest.mock('@walletconnect/jsonrpc-provider', () => ({
 
 describe('getBestUrl', () => {
   test('works with a single string', async () => {
-    const rpc = await getBestUrlMap({0: 'succeed_0'}, 100)
+    const rpc = await getBestUrlMap({ 0: 'succeed_0' }, 100)
     expect(rpc[0]).toBe('succeed_0')
   })
 
   test('works with 1 rpc (success)', async () => {
-    const rpc = await getBestUrlMap({0: ['succeed_0']}, 100)
+    const rpc = await getBestUrlMap({ 0: ['succeed_0'] }, 100)
     expect(rpc[0]).toBe('succeed_0')
   })
 
   test('works with 2 urls (success/failure)', async () => {
-    const rpc = await getBestUrlMap({0: ['succeed_0', 'fail_0']}, 100)
+    const rpc = await getBestUrlMap({ 0: ['succeed_0', 'fail_0'] }, 100)
     expect(rpc[0]).toBe('succeed_0')
   })
 
   test('works with 2 urls (failure/success)', async () => {
-    const rpc = await getBestUrlMap({0: ['fail_0', 'succeed_0']}, 100)
+    const rpc = await getBestUrlMap({ 0: ['fail_0', 'succeed_0'] }, 100)
     expect(rpc[0]).toBe('succeed_0')
   })
 
   test('works with 2 successful urls (fast/slow)', async () => {
-    const rpc = await getBestUrlMap({0: ['succeed_0', 'succeed_1']}, 100)
+    const rpc = await getBestUrlMap({ 0: ['succeed_0', 'succeed_1'] }, 100)
     expect(rpc[0]).toBe('succeed_0')
   })
 
   test('works with 2 successful urls (slow/fast)', async () => {
-    const rpc = await getBestUrlMap({0: ['succeed_1', 'succeed_0']}, 100)
+    const rpc = await getBestUrlMap({ 0: ['succeed_1', 'succeed_0'] }, 100)
     expect(rpc[0]).toBe('succeed_1')
   })
 
   test('works with 2 successful urls (after timeout/before timeout)', async () => {
-    const rpc = await getBestUrlMap({0: ['succeed_100', 'succeed_0']}, 50)
+    const rpc = await getBestUrlMap({ 0: ['succeed_100', 'succeed_0'] }, 50)
     expect(rpc[0]).toBe('succeed_0')
   })
 })
