c383be1a930824c3a5bb3c7df190cdd8a82dfc7c
Eddie Dugan <eddie.dugan@uniswap.org>
2023-06-15 11:35:17 -0700

    test: testing signTypedData

--- example/components/connectorCards/WalletConnectV2Card.tsx
+++ example/components/connectorCards/WalletConnectV2Card.tsx
@@ -1,9 +1,25 @@
+import { _TypedDataEncoder } from '@ethersproject/hash'
+import { AllowanceTransfer, MaxAllowanceTransferAmount, PERMIT2_ADDRESS, PermitSingle } from '@uniswap/permit2-sdk'
+import { UNIVERSAL_ROUTER_ADDRESS } from '@uniswap/universal-router-sdk'
 import { URI_AVAILABLE } from '@web3-react/walletconnect-v2'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { MAINNET_CHAINS } from '../../chains'
 import { hooks, walletConnectV2 } from '../../connectors/walletConnectV2'
 import { Card } from '../Card'
+
+interface Permit extends PermitSingle {
+  sigDeadline: number
+}
+
+// 30 days in milliseconds
+const PERMIT_EXPIRATION = 1000 * 60 * 60 * 24 * 30
+// 30 minutes in milliseconds
+const PERMIT_SIG_EXPIRATION = 1000 * 60 * 30
+
+function toDeadline(expiration: number): number {
+  return Math.floor((Date.now() + expiration) / 1000)
+}
 
 const CHAIN_IDS = Object.keys(MAINNET_CHAINS).map(Number)
 
@@ -35,18 +51,44 @@ export default function WalletConnectV2Card() {
     })
   }, [])
 
+  const signMessage = useCallback(async () => {
+    if (!provider) {
+      return
+    }
+    const permit: Permit = {
+      details: {
+        token: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI mainnet
+        amount: MaxAllowanceTransferAmount,
+        expiration: toDeadline(PERMIT_EXPIRATION),
+        nonce: 100, // high number to not conflict with current allowance
+      },
+      spender: UNIVERSAL_ROUTER_ADDRESS(chainId),
+      sigDeadline: toDeadline(PERMIT_SIG_EXPIRATION),
+    }
+    console.log(provider.getSigner())
+    const { domain, types, values } = AllowanceTransfer.getPermitData(permit, PERMIT2_ADDRESS, chainId)
+    const address = (await provider.getSigner().getAddress()).toLowerCase()
+    const message = JSON.stringify(_TypedDataEncoder.getPayload(domain, types, values))
+    const signature = await await provider.getSigner().provider.send('eth_signTypedData', [address, message])
+
+    console.log(`signature: ${signature}`)
+  }, [chainId, provider])
+
   return (
-    <Card
-      connector={walletConnectV2}
-      activeChainId={chainId}
-      chainIds={CHAIN_IDS}
-      isActivating={isActivating}
-      isActive={isActive}
-      error={error}
-      setError={setError}
-      accounts={accounts}
-      provider={provider}
-      ENSNames={ENSNames}
-    />
+    <>
+      <Card
+        connector={walletConnectV2}
+        activeChainId={chainId}
+        chainIds={CHAIN_IDS}
+        isActivating={isActivating}
+        isActive={isActive}
+        error={error}
+        setError={setError}
+        accounts={accounts}
+        provider={provider}
+        ENSNames={ENSNames}
+      />
+      <button onClick={signMessage}>test signature</button>
+    </>
   )
 }
