81f24432d5f38cefced223cd057f09aa0e575c08
circlehotarux <circlehotarux@gmail.com>
2024-01-16 22:09:09 +0800

    fix: example wc2 config

--- example/connectors/walletConnectV2.ts
+++ example/connectors/walletConnectV2.ts
@@ -3,7 +3,8 @@ import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
 
 import { MAINNET_CHAINS } from '../chains'
 
-const [mainnet, ...optionalChains] = Object.keys(MAINNET_CHAINS).map(Number)
+const optionalChains = Object.keys(MAINNET_CHAINS).map(Number)
+const [mainnet] = optionalChains
 
 export const [walletConnectV2, hooks] = initializeConnector<WalletConnectV2>(
   (actions) =>
